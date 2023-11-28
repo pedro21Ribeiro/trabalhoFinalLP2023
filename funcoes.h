63fc1c0085d7ed80190e8b2ff08736a60d285c0e
Pedro Silva Ribeiro <pedr21ribeiro7@gmail.com>
2023-11-28 20:35:16 -0300

    Mudanças

--- funcoes.h
+++ funcoes.h
@@ -28,3 +28,8 @@ extern p_contas usuarios[MAX];
 
 int popularVetor(char *arquivo);
 int adicionarUsuarios(char *arquivo, char nome[tamNome], int tipoDeConta, double saldo);
+Conta retornoUsers(char query[600]);
+Movimento retornoMovimento(char query[600]);
+void adicionarUser(char nome[51], int tipoDeConta, double saldo);
+void adicionarMovimento(int sender, int reciver, double valor, char data[11]);
+void usersSemRetorno(char query[600]);
